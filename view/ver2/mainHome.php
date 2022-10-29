45baa73ff7a19a997d983f700455cc0aaecf2f6a
Elian <elian.alvarado.2004@gmail.com>
2022-10-28 17:03:43 -0700

    Aplicación de otra vista

---
+++ view/ver2/mainHome.php
@@ -0,0 +1,38 @@
+<div class="container text-center mainbox p-3">
+    <h1 class="m-3">Bienvenido al Sistema de Inventario Amazing Inventions</h1>
+</div>
+
+<div class="container my-5 row">
+
+    <div class="container col">
+        <div class="cleanslate w24tz-current-time w24tz-middle"
+        style="display: inline-block !important; visibility: hidden !important; min-width:300px !important; min-height:145px !important;">
+        <p>
+            <a href="//24timezones.com/Guatemala/hora" style="text-decoration: none" class="clock24"
+                id="tz24-1666999657-cc12889-eyJob3VydHlwZSI6IjEyIiwic2hvd2RhdGUiOiIxIiwic2hvd3NlY29uZHMiOiIwIiwiY29udGFpbmVyX2lkIjoiY2xvY2tfYmxvY2tfY2I2MzVjNjU2OWJiOWJkIiwidHlwZSI6ImRiIiwibGFuZyI6ImVzIn0="
+                title="Hora en Guatemala" target="_blank" rel="nofollow">
+                Hora actual en Guatemala</a>
+        </p>
+        <div id="clock_block_cb635c6569bb9bd"></div>
+
+        </div>
+        <script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
+    </div>
+
+    <div class="container col dashbox text-center py-2 mx-2">
+        <h5 class="my-5">Administrar Material</h5>
+    </div>
+
+    <div class="container col dashbox text-center py-2 mx-2">
+        <h5 class="my-5">Administrar Usuarios</h5>
+    </div>
+
+    <div class="container col dashbox text-center py-2 mx-2">
+        <h5 class="my-5">Administrar Equipo</h5>
+    </div>
+</div>
+
+<div class="container text-end my-5">
+    <span><img src="../../assets/img/logoprod.png" alt="">
+    </span>
+</div>
